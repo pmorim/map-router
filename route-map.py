88e81a0f88d67deba1cae79f88b17c883ffa9932
Pedro Morim <1180798@isep.ipp.pt>
2021-11-07 23:49:55 +0000

    Added verbose

--- route-map.py
+++ route-map.py
@@ -16,15 +16,19 @@ def route_map(dest_csv, routes_csv, map_img, out_img, size_ratio):
     """Draw individual routes on a given map."""
 
     # Plotting settings
+    click.echo("Applying initial styles... ", nl=False)
     plt.figure(dpi=300)
     sns.set_style('white')
     sns.set_context('talk')
+    click.echo('Done')
 
     # Read the given data
+    click.echo("Reading the given data... ", nl=False)
     coords = pd.read_csv(dest_csv, index_col='Destination')
     routes = pd.read_csv(routes_csv, header=None)
     routes.fillna('-', inplace=True)  # Replace 'NaN' with '-'
     img = Image.open(map_img)
+    click.echo('Done')
 
     # Initialize the plot
     width, height = img.size
@@ -32,41 +36,47 @@ def route_map(dest_csv, routes_csv, map_img, out_img, size_ratio):
     plt.figure(figsize=figsize)
 
     # Draw the map and destinations
+    click.echo("Drawing the destination points... ", nl=False)
     plot = sns.scatterplot(data=coords, x='x', y='y', color='black')
     plot.imshow(img)
-
-    # Close the original map image
-    img.close()
+    click.echo('Done')
 
     # Iterate through all routes
-    for _, route in routes.iterrows():
-        # Create a DataFrame for the route
-        df = pd.DataFrame(columns=['x', 'y'])
+    with click.progressbar(
+        routes.iterrows(),
+        length=len(routes.index),
+        label="Drawing the routes..."
+    ) as bar:
+        for _, route in bar:
+            # Create a DataFrame for the route
+            df = pd.DataFrame(columns=['x', 'y'])
 
-        # Iterate through all locations in the route
-        for location in route:
-            if location != '-':
-                df = df.append(coords.loc[location])
+            # Iterate through all locations in the route
+            for location in route:
+                if location != '-':
+                    df = df.append(coords.loc[location])
 
-        # Convert coordinates to integers
-        df = df.astype({'x': int, 'y': int})
+            # Convert coordinates to integers
+            df = df.astype({'x': int, 'y': int})
 
-        # Draw the route
-        plot.plot('x', 'y', '', data=df)
+            # Draw the route
+            plot.plot('x', 'y', '', data=df)
 
-    # Remove axis
+    # Add final style touches
+    click.echo("Applying final styles... ", nl=False)
     plot.set(xticklabels=[], yticklabels=[], xlabel=None, ylabel=None)
     sns.despine(left=True, bottom=True)
-
-    # Set the correct size and ratio
     plot.set_xlim(0, width)
     plot.set_ylim(height, 0)
+    click.echo('Done')
 
     # Save the final image
+    click.echo(f"Saving output as \"{out_img}\"... ", nl=False)
     plt.savefig(out_img, pad_inches=0, bbox_inches='tight')
+    click.echo('Done')
 
-    # Signal end of script
-    click.echo('Routes drawn successfully')
+    # Close the original map
+    img.close()
 
 
 if __name__ == '__main__':
